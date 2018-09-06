f5733a7da361bafacbb3a4d05a3a295c4b731e4b
Rachel Wyatt <rw126@evansville.edu>
2018-09-06 23:17:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,5 @@ def find_the_cheese(string_array)#
   cheese_types = ["cheddar", "gouda", "camembert"]
   if string_array.include?("cheddar") || ("gouda")
     return "cheddar"
-  else
-    nil
   end
 end

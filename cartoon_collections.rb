c6b3ec42083187b94781e93aded94b764fa3b84d
Rachel Wyatt <rw126@evansville.edu>
2018-09-06 23:03:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, index|
+    indexplusone = index + 1
+    puts "#{indexplusone} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)# code an argument here
+  #veggies.collect {|x| x.upcase + "!"}
+  veggies.map {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here

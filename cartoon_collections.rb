1d100056190fd40c079a4d81b55b5a9a86b64c9f
Rachel Wyatt <rw126@evansville.edu>
2018-09-06 23:10:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(call_array)
   call_array.any? {|word| word.length < 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(string_array)#
+  string_array.include? (cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
